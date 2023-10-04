1a8d02ab6f3c67d51d594b918905e3ddb8ea9f14
mohammed_shuqair <eng.mohammed.shuqair@gmail.com>
2023-10-04 19:45:23 +0300

    Initial commit

---
+++ app/src/test/java/selenium2/Constants.java
@@ -0,0 +1,7 @@
+package selenium2;
+
+public class Constants {
+    public static String baseUrl="file:///D:/college/Y5_S1/Advanced software engineering/selenium/Selenium Resources-20231003/selenium2/app/src/test/resources/";
+    public static String kT3Link=baseUrl+"LinksExample.htm";
+    public static String kT3Option=baseUrl+"SecondOption.htm";
+}
